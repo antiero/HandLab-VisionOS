bedd2261a86af443507294352bd104c023425911
Antony Nasce <nasce@mac.com>
2025-11-15 09:09:28 +0000

    Initial Commit

---
+++ HandLab/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  HandLab
+//
+//  Created by Antony Nasce on 15/11/2025.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ HandLab/HandLabApp.swift
@@ -0,0 +1,33 @@
+//
+//  HandLabApp.swift
+//  HandLab
+//
+//  Created by Antony Nasce on 15/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct HandLabApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
